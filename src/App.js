ec157da4f6f11cc7e6c771963df3ecf0f211f9bd
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-01-27 23:00:00 +0530

    Added landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
+<<<<<<< HEAD
 import './App.css';
+=======
+import './App.scss';
+>>>>>>> 3891cf4 (Added Logo and arrow in landing page)
 import Home from './components/home/Home';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div >
      <Home/>
+=======
+    <div className='App'>
+     <section><Home/></section>
+     <section>hi </section>
+     <section>hi </section>
+     
+
+>>>>>>> 3891cf4 (Added Logo and arrow in landing page)
     </div>
   );
 }
