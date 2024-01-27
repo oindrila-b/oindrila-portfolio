7b7fd12849fcadf5094aca391218dd9c2ac4e4cf
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2024-01-27 23:01:37 +0530

    Added landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-<<<<<<< HEAD
-import './App.css';
-=======
+
 import './App.scss';
->>>>>>> 3891cf4 (Added Logo and arrow in landing page)
 import Home from './components/home/Home';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div >
-     <Home/>
-=======
     <div className='App'>
      <section><Home/></section>
      <section>hi </section>
      <section>hi </section>
      
-
->>>>>>> 3891cf4 (Added Logo and arrow in landing page)
     </div>
   );
 }
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import '../logo/logo.scss'
-<<<<<<< HEAD
-=======
 import logo from '../../assets/logo.png'
 import down from '../../assets/down.png'
->>>>>>> 3891cf4 (Added Logo and arrow in landing page)
+
 
 const Logo = () => {
   return (
     <div className='logo-container'>
-<<<<<<< HEAD
-
-=======
       <img src={logo} alt='logo'/> 
       <div className='down'>
         <img src={down} alt=''/> 
       </div>
->>>>>>> 3891cf4 (Added Logo and arrow in landing page)
     </div>
   )
 }
